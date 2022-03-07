70f291bf7d0e576bbd7497bc7ac999e29f4572b1
Omar A. Alhammadi <emarati.no1@gmail.com>
2022-03-07 20:12:27 +0400

    small adjustments to EpisodeItem and Episodelist.

--- components/EpisodeItem.js
+++ components/EpisodeItem.js
@@ -3,11 +3,11 @@ import { useCallback, useRef } from "react"
 export default function EpisodeItem({ data }) {
     return <a href="https://www.youtube.com/embed/ScMzIvxBSi4" className="select-none flex flex-col rounded border p-4 bg-white hover:from-orange-400 hover:bg-gradient-to-l hover:to-red-500 hover:text-white  cursor-pointer space-y-2 max-h-80" >
         <div className="flex flex-col sm:flex-row space-x-0 sm:space-x-4 text-xl font-bold">
-            <div>Episode {data.number}</div><h1>{data.title}</h1>
+            <div>Episode {data.number.split(" ")[2]}</div><h1>{data.title}</h1>
         </div>
         <div className="text-sm">Writer(s): {data.writers} - Airing Date: {data.originalAirDate}</div>
-        <p className="overflow-auto">
+        <div className="text-clip text-sm">
             {data.desc}
-        </p>
+        </div>
     </a>
 }
--- components/EpisodeList.js
+++ components/EpisodeList.js
@@ -4,6 +4,6 @@ import EpisodeItem from './EpisodeItem';
 
 export default function EpisodeList({ episodes }) {
     return (
-        <div className="flex flex-col space-y-2 p-2 h-screen overflow-y-scroll">{episodes.map((v) => <EpisodeItem data={v}></EpisodeItem>)}</div>
+        <div className="flex flex-col space-y-2 p-2 overflow-y-scroll">{episodes.map((v) => <EpisodeItem data={v}></EpisodeItem>)}</div>
     )
 }
